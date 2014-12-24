d8ce1df5de94489c60246f562366f2131a614e3e
Bill Neuson <bneuson@gmail.com>
2014-12-24 01:17:09 -0600

    Added UI elements, IP detection
    UI elements added, device IP detection implemented for network scan.

--- app/src/main/java/com/kc9zyz/cc3200wifiledblinky/MainActivity.java
+++ app/src/main/java/com/kc9zyz/cc3200wifiledblinky/MainActivity.java
@@ -1,9 +1,24 @@
 package com.kc9zyz.cc3200wifiledblinky;
 
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.format.Formatter;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import org.apache.http.HttpResponse;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+
+import java.io.IOException;
+import java.net.InetAddress;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +27,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText mEdit   = (EditText)findViewById(R.id.editText);
+        WifiManager wifiMgr = (WifiManager) getSystemService(WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
+        int ip = wifiInfo.getIpAddress();
+        String ipString = String.format(
+                "%d.%d.%d.",
+                (ip & 0xff),
+                (ip >> 8 & 0xff),
+                (ip >> 16 & 0xff));
+        mEdit.setText(ipString);
     }
 
 
@@ -35,5 +60,28 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void scanIP(View view)
+    {
+        int ipSuffix = 0;
+        WifiManager wifiMgr = (WifiManager) getSystemService(WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
+        int ip = wifiInfo.getIpAddress();
+        String ipString = String.format(
+                "%d.%d.%d.",
+                (ip & 0xff),
+                (ip >> 8 & 0xff),
+                (ip >> 16 & 0xff));
+
+        HttpClient client = new DefaultHttpClient();
+         while(ipSuffix < 256) {
+             try {
+                 HttpPost post = new HttpPost(ipString + ipSuffix);
+                 HttpResponse response = client.execute(post);
+
+             } catch (IOException e) {
+                 ipSuffix++;
+             }
+         }
     }
 }
